3797abad5befd54478c8dfc272883cc5fcd9b7e9
GQuiroz <74075209+GabrielQuiroz1995@users.noreply.github.com>
2023-06-14 12:16:20 -0400

    Api sin proxy,funcionando

---
+++ src/main/java/cl/forum/arq/bts/service/PokeApiClientPrueba.java
@@ -0,0 +1,27 @@
+package cl.forum.arq.bts.service;
+
+import cl.forum.arq.bts.util.Pokemon;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+
+public class PokeApiClientPrueba {
+
+    private static final String BASE_URL = "https://pokeapi.co/api/v2";
+
+    @GET
+    @Path("/pokemon")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Pokemon getPokemon(@QueryParam("name") String name) {
+        Client client = ClientBuilder.newClient();
+        return client.target(BASE_URL)
+                .path("/pokemon/" + name)
+                .request(MediaType.APPLICATION_JSON)
+                .get(Pokemon.class);
+    }
+}
